9ed4244af88731905d94d5449dbb0a67cdd7376e
v1Ews <72022438+view102@users.noreply.github.com>
2022-02-06 16:21:17 +0700

    GET2 AND DELETE

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ class Reservation(BaseModel):
 client = MongoClient('mongodb://localhost', 27017)
 
 # TODO fill in database name
-db = client["<put your database name>"]
+db = client["mini_exceed13"]
 
 # TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["Reservation"]
 
 app = FastAPI()
 
@@ -23,9 +23,23 @@ app = FastAPI()
 def get_reservation_by_name(name:str):
     pass
 
-@app.get("reservation/by-table/{table}")
+@app.get("/reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
-    pass
+    query = {"table_number": table}
+    result = collection.find(query, {"_id": 0, "name": 1, "time": 1, "table_number": 1})
+    my_result = []
+    for r in result:
+        my_result.append(r)
+    # print(my_result)
+    if len(my_result) == 0:
+        return {
+            "result": "Not Found."
+        }
+    else:
+        return {
+            "result": my_result
+        }
+
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
@@ -37,5 +51,9 @@ def update_reservation(reservation: Reservation):
 
 @app.delete("/reservation/delete/{name}/{table_number}")
 def cancel_reservation(name: str, table_number : int):
-    pass
+    query = {"name": name, "table_number": table_number}
+    collection.delete_many(query)
+    return {
+        "result": "done."
+    }
 
