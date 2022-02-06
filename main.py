6c58098f765ab84fd533f7b1a62840d638b8acfd
AiemChanavit <47749176+AiemChanavit@users.noreply.github.com>
2022-02-06 16:19:58 +0700

    get1

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from http import client
+import imp
+from types import new_class
 from pymongo import MongoClient
+
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
+from typing import Optional
+from fastapi.encoders import jsonable_encoder
 
 class Reservation(BaseModel):
     name : str
@@ -10,10 +16,10 @@ class Reservation(BaseModel):
 client = MongoClient('mongodb://localhost', 27017)
 
 # TODO fill in database name
-db = client["<put your database name>"]
+db = client["mini_exceed13"]
 
 # TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["Reservation"]
 
 app = FastAPI()
 
@@ -21,6 +27,18 @@ app = FastAPI()
 # TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
 def get_reservation_by_name(name:str):
+    result = collection.find({"name": name}, {"_id":0})
+    res = []
+    for r in result:
+        res.append(r)
+    print(res)
+    if result != None:
+        return {
+            "status": "found",
+            "result": result
+        }
+    else:
+        raise HTTPException(404, f"Couldn't find reservation with name: {name}'")
     pass
 
 @app.get("reservation/by-table/{table}")
